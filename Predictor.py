52713d47822dd4df0b3de7fec92a75aeeb6f1283
breadbread1984 <breadbread1984@163.com>
2019-07-22 17:23:13 +0800

    update predictor

--- Predictor.py
+++ Predictor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os;
+import csv;
 import tensorflow as tf;
 from BERT import BERT;
 
@@ -9,6 +11,42 @@ class Predictor(object):
 
         # create bert object
         self.bert, self.tokenizer = BERT(max_seq_len = max_seq_len);
+
+    def read_tsv(self, inputfile):
+
+        with tf.io.gfile.GFile(inputfile, "r") as f:
+            reader = csv.reader(f, delimiter = '\t');
+            lines = [];
+            for line in reader:
+                lines.append(line);
+            return lines;
+
+    def create_dataset(self, examples):
+
+        dataset = [];
+        for line in examples:
+            label = line[0];
+            question = line[1];
+            answer = line[2];
+            dataset.append((question,answer,label));
+        return dataset;
+
+    def get_examples(self, data_dir = None):
+
+        assert type(data_dir) is str;
+        train_examples = self.read_tsv(os.path.join(data_dir, "train.tsv"));
+        test_examples = self.read_tsv(os.path.join(data_dir, "test.tsv"));
+        trainset = self.create_dataset(train_examples);
+        testset = self.create_dataset(test_examples);
+        return trainset, testset;
+
+    def train(self, data_dir = None, batch = 32, epoch = 3):
+
+        assert type(data_dir) is str;
+        trainset, testset = self.get_examples(data_dir);
+        num_train_steps = int(len(trainset) / batch * epoch);
+        num_warmup_steps = int(num_train_steps * 0.1);
+        # TODO
 
 if __name__ == "__main__":
 
